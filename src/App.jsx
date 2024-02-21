a86395593699bb1d11fa0dfdd9bc234392f5a4b5
srxjoao <139173784+srxjoao@users.noreply.github.com>
2024-02-21 16:04:38 -0400

    modificações feitas em aulas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-export default function App() {
-
-  const nome = "João Santos";
-  
+export default function App(){
+  const nome = "João Pedro";
+  let myname = "João Pedro De Oliveira";
   return(
     <div>
         <h1>Hello World</h1>
         <p>Desenvolvido por: {nome}</p>
+        <p>Meu paragrafo:{myname}</p>
     </div>  
   );
 
