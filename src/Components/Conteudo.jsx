89d6e27189aeda2fd25ab12a879593cd26d0b95f
srxjoao <139173784+srxjoao@users.noreply.github.com>
2024-03-12 08:16:28 -0400

    atualização no componente conteudo

--- src/Components/Conteudo.jsx
+++ src/Components/Conteudo.jsx
@@ -1,8 +1,20 @@
 export default function Conteudo (){
     return(
-        <div>
-            <h2>React Tests</h2>
-            <p>Minha primeira pagina com react</p>
-        </div>
+        <main>
+            <div className="oqr" >
+                <h3>O que é react?</h3>
+                <ul>
+                O React é uma biblioteca criada para construir telas de forma declarativa. Ou seja: o React cria aplicações web para serem executadas em navegadores, sejam eles para desktop, mobile ou qualquer outro dispositivo.
+                </ul>
+            </div>
+            
+            <div  className="oqr2">
+                <h3>O que é uma SPA (Single Page Application)?</h3>
+                <ul>
+                SPA (Single Page Application) é uma aplicação que carrega apenas um único documento Web e, em seguida, atualiza o conteúdo do corpo desse documento único por meio de APIs JavaScript.
+                </ul>
+            </div>
+
+        </main>
     );
 }
