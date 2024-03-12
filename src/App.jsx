24c3dc213f1b9e9d81b5b0f93b8511c1fe67ca75
srxjoao <139173784+srxjoao@users.noreply.github.com>
2024-03-12 08:39:14 -0400

    Aula Remota-12/03

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Footer from "./Components/Footer";
 import Header  from "./Components/Header";
-import Conteudo from "./Components/Conteudo"
+import Conteudo from "./Components/Conteudo";
+import './globals.css'
 export default function App(){
   return(
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,11 +3,11 @@ export default function Header(){
         <header>
             <h1>PTAC</h1>
             <nav>
-                <ul>
+                <li>
                     <li><a src="#">Home</a></li>
                     <li><a src="#">Sobre</a></li>
                     <li><a src="#">Contato</a></li>
-                </ul>
+                </li>
             </nav>
         </header>
     );
